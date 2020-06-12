5ed21d308378dd63edb8521cc68b38fa59f2f4ab
arnavpuri <arnavanytime@gmail.com>
2020-06-12 19:28:14 +0530

    Updates

---
+++ precache-manifest.fb8229bdc98daa9e3b02eaf318e085ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae31d9bb4c323db60f3e4ee25661b39a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2770577e5e96c2b1b55",
+    "url": "/static/css/main.002a29d7.chunk.css"
+  },
+  {
+    "revision": "9686f8f412547ce08b05",
+    "url": "/static/js/2.9b77d22c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.9b77d22c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2770577e5e96c2b1b55",
+    "url": "/static/js/main.b1d1e358.chunk.js"
+  },
+  {
+    "revision": "5394519942792e0985a1",
+    "url": "/static/js/runtime-main.dd532961.js"
+  }
+]);
